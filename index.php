b0f45dda0911133d411c0ce3730e5e77d422d3c4
aryawardhana15 <wardhanahafiz567@gmail.com>
2025-06-13 22:49:11 +0700

    add fitur

--- index.php
+++ index.php
@@ -1598,7 +1598,7 @@ if ($auth->isLoggedIn()) {
         </div>
     </section>
 
-    <!-- Footer -->
+
     <footer class="bg-dark text-white py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
